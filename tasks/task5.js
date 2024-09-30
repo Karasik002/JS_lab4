aa48fd2f610314a284d3136716ec9f25a47b6e5d
Марина Білошицька <kn23-m.biloshytska@nubip.edu.ua>
2024-09-30 20:51:50 +0300

    Коміт 5 завдання

--- tasks/task5.js
+++ tasks/task5.js
@@ -3,11 +3,20 @@
 
 
 function countOccurrences(str, char) {
-  //Ваш код
+  let count = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === char) {
+      count++;
+    }
+  }
+
+  return count;
 }
 
-// Приклади використання:
-console.log(countOccurrences("hello world", 'o')); // Виведе: 2
-console.log(countOccurrences("banana", 'a'));       // Виведе: 3
+console.log(countOccurrences("hello world", 'o')); 
+console.log(countOccurrences("banana", 'a'));      
+console.log(countOccurrences("example", 'x'));
+console.log(countOccurrences("hello", 'z'));
 
 module.exports = countOccurrences;

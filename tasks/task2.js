2aefee67f33c33544f99c52f3e67e593c634dfde
Марина Білошицька <kn23-m.biloshytska@nubip.edu.ua>
2024-10-03 12:01:54 +0300

    другий коміт з 2 лабки

--- tasks/task2.js
+++ tasks/task2.js
@@ -2,17 +2,24 @@
 //  при цьому пропускаючи всі цифри.
 
 function reverseStringWithoutDigits(str) {
-  let filteredStr = str.replace(/\d/g, '');
-  let reversedStr = filteredStr.split('').reverse().join('');
+  let filteredStr = '';
+  for (let i = 0; i < str.length; i++) {
+    if (isNaN(str[i]) || str[i] === ' ') {
+      filteredStr += str[i];
+    }
+  }
+  let reversedStr = '';
+  for (let i = filteredStr.length - 1; i >= 0; i--) {
+    reversedStr += filteredStr[i];
+  }
   return reversedStr;
 }
-
 let input = "abc123def456";
 let result = reverseStringWithoutDigits(input);
-console.log(result); 
+console.log(result);
 
-console.log(reverseStringWithoutDigits("hello5world")); 
-console.log(reverseStringWithoutDigits("abc123xyz"));        
+console.log(reverseStringWithoutDigits("hello5world"));
+console.log(reverseStringWithoutDigits("abc123xyz"));
 console.log(reverseStringWithoutDigits("123"));
 console.log(reverseStringWithoutDigits(""));
 console.log(reverseStringWithoutDigits("abcdef"));
@@ -20,3 +27,4 @@ console.log(reverseStringWithoutDigits("hello"));
 console.log(reverseStringWithoutDigits("1234567890"));
 
 module.exports = reverseStringWithoutDigits;
+
